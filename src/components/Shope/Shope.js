f34b2464383e6136873ac188d4147f2522999c2d
unknown <taha.iu.bd@gmail.com>
2022-03-25 23:51:38 +0600

    slected-product-style-add

--- src/components/Shope/Shope.js
+++ src/components/Shope/Shope.js
@@ -10,14 +10,14 @@ const Shope = () => {
             .then((data) => setProducts(data))
     }, [])
     return (
-        <div className="shope-container m-5 ">
+        <div className="shope-container ms-5 ">
             <div className="card-container row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5 mt-5">
                 {products.map((product) => (
                     <Products key={product.id} products={product}></Products>
                 ))}
             </div>
-            <div className="select-container ms-5 mt-5">
-                <h4 className=" text-center">Slected</h4>
+            <div className="selected-container ms-5 mt-5">
+                <h4 className=" text-center mt-5">Selected Bike</h4>
             </div>
         </div>
     )
