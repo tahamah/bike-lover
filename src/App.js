ad3e308abdec84010163e0fe183f368e36565828
unknown <taha.iu.bd@gmail.com>
2022-03-26 13:17:41 +0600

    footer add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Shope from './components/Shope/Shope'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopyright } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
     return (
-        <div className="container bg-white rounded mb-5 pb-5">
-            <Header></Header>
-            <Shope></Shope>
+        <div>
+            <div className="container bg-white rounded mb-5 pb-5">
+                <Header></Header>
+                <Shope></Shope>
+            </div>
+            <footer className="text-center footer ">
+                <p className="mb-0 ">
+                    <FontAwesomeIcon icon={faCopyright} /> copyright 2022 bike
+                    lover
+                </p>
+            </footer>
         </div>
     )
 }
