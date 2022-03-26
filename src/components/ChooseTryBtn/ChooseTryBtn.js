27c5b4b87e5f59e223cecdae27b0f652089418a7
unknown <taha.iu.bd@gmail.com>
2022-03-26 12:12:12 +0600

    model add

--- src/components/ChooseTryBtn/ChooseTryBtn.js
+++ src/components/ChooseTryBtn/ChooseTryBtn.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import Modal from 'react-modal'
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+}
+Modal.setAppElement('#root')
 
 const ChooseTryBtn = ({ handleTryAgain, handleChooseOne, chooseOne }) => {
     console.log(chooseOne)
+    const [modalIsOpen, setIsOpen] = React.useState(false)
+
+    function openModal() {
+        setIsOpen(true)
+    }
+
+    function closeModal() {
+        setIsOpen(false)
+    }
     return (
         <div>
+            <button onClick={openModal}>Open Modal</button>
             <button
                 className="btn mb-2 btn-outline-success"
                 onClick={handleChooseOne}
@@ -17,6 +39,15 @@ const ChooseTryBtn = ({ handleTryAgain, handleChooseOne, chooseOne }) => {
             >
                 Try Again
             </button>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <button onClick={closeModal}>close</button>
+                <h1>Done</h1>
+            </Modal>
         </div>
     )
 }
